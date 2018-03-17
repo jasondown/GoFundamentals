335b86cd7a38d8fd105ec7a2f7e66c030dd7728f
Jason Down <jdown@igloosoftware.com>
2018-03-16 21:23:27 -0400

    Added Module 7 loops self-destruct code.

---
+++ loops/self-destruct.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	for timer := 10; timer >= 0; timer-- {
+		if timer == 0 {
+			fmt.Println("BOOOOOOOOOOOOOOOOOOOOOM!")
+			break
+		}
+		fmt.Println(timer)
+		time.Sleep(1 * time.Second)
+	}
+}
